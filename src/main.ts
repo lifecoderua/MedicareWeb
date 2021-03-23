6ab8582aef6fd594871459d0789acea1d7553926
lifecoderua <lifecoder.ua@gmail.com>
2021-03-23 18:13:59 +0200

    Vuefire and Firestore integration

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,27 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import firebase from "firebase";
+import { firestorePlugin } from 'vuefire'
+
+firebase.firestore().enablePersistence()
+  .catch((err) => {
+    if (err.code == 'failed-precondition') {
+      console.log('Multiple application tabs open. Firestore persistence not enabled.');
+    } else if (err.code == 'unimplemented') {
+      console.error('Firestore Persistence is not supported.');
+    }
+  });
+
 
 Vue.config.productionTip = false;
+Vue.use(firestorePlugin);
+
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
 }).$mount("#app");
+
+
