a384e81a4f7cc8db86637c1e691917b4a8e8a735
lifecoderua <lifecoder.ua@gmail.com>
2021-07-27 11:42:55 +0300

    Vuefire and Firestore - run fixed

--- src/services/db.ts
+++ src/services/db.ts
@@ -4,11 +4,17 @@ import "firebase/firestore";
 let dbInstance!: firebase.firestore.Firestore;
 
 export function init() {
+  if (dbInstance) {
+    return;
+  }
+
   dbInstance = firebase.initializeApp({ projectId: "medicare-4ccfe" }).firestore();
 }
 
 const DBService = {
   get db() {
+    init();
+
     return dbInstance;
   }
 }
